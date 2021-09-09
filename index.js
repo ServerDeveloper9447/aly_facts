8c4a771b87e0546bb535438f55917e80c7f298bc
Soumalya Ghosh <86161882+ServerDeveloper9447@users.noreply.github.com>
2021-09-09 22:36:28 +0530

    New facts + New functions

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const list = `Wants to learn C++,Knows HTML a little,Has a PS5, with no games in it,Has a low end device, which she always uses except high end which she never use,Used to hangout, but now cannot,Always falls to the ground because medicine reactions,Found out Discord is a bit weird,Dies after she heard djsv13 is hard,Low brain memory, cannot remember stuff,cries all time,Takes medicinefull syringe all day,Doesn't eat much,Doesn't sleep at all until takes medicine,Lost a bf cuz he cheat,Sometimes goes mental,Always sad,Lives alone in a warehouse,Has nothing but a pet cat,She likes sex idk why,always sad,does not know how npms work,noob at coding so working with aoi,Weak memory, so can't remember anything,that's my age (till oct-9),always hungry but does not eat,Overdose on sleep medicines,Can't understand DJS,uh sleep medicines are my life,have no friends`.split(',');
+const list = `Wants to learn C++,Knows HTML a little,Has a PS5 with no games in it,Has a low end device which she always uses except high end which she never use,Used to hangout but now cannot,Always falls to the ground because medicine reactions,Found out Discord is a bit weird,Dies after she heard djsv13 is hard,Low brain memory, Cannot remember stuff,Cries all time,Takes medicinefull syringe all day,Doesn't eat much,Doesn't sleep at all until takes medicine,Lost a bf cuz he cheat,Sometimes goes mental,Always sad,Lives alone in a warehouse,Has nothing but a pet cat,She likes sex idk why,Always sad,Does not know how npms work,Noob at coding so working with aoi,Weak memory so can't remember anything,Always hungry but does not eat,Overdose on sleep medicines,Can't understand DJS,Uh sleep medicines are my life,Has no friends`.split(',');
 
-function fact(){
+function getFact(){
     const facts = [list], question_pack = facts[Math.floor(Math.random() * facts.length)], question = question_pack[Math.floor(Math.random() * question_pack.length)];
     return question;
   }
 
+function getAllFacts(){
+  const facts = [list]
+  return facts;
+  }
+
+function version(){
+  const version = require(__dirname + '/package.json').version
+  return version;
+  }
+
 module.exports = {
-    fact
+  getFact,
+  getAllFacts,
+  version
 }
